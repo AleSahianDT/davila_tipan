37b5845645b122462438225a330cbd69bb1a2692
Alejandra.D <alejandra.davila.tipan@udla.edu.ec>
2024-05-02 15:31:00 -0500

    Main Me olvide

--- src/Ventana.java
+++ src/Ventana.java
@@ -37,4 +37,12 @@ public class Ventana {
             }
         });
     }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ventana");
+        frame.setContentPane(new Ventana().People);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
